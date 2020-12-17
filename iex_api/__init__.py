5d4d1a52d701d173302ce7be043bc5c0e08b81d0
Thomas Kluiters <thomas.kluiters@gmail.com>
2020-12-17 19:27:12 +0100

    Initial commit

---
+++ iex_api/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for iex-api."""
+
+__author__ = "Volpyx"
+__email__ = "thomas.kluiters@gmail.com"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.0"
+
+
+def get_module_version():
+    return __version__
+
+
+from .example import Example  # noqa: F401
